fc8b12fbf51f0e22ecebb81d8ac1ff919a57098e
Mike Harris <mikeharris@Macintosh.local>
2015-08-27 13:35:18 -0700

    Twilio can send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,15 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    account_sid = 'AC6650453086e3f6940bf702113274bcbe'
+    auth_token = 'bdae3003953d3658634b3bbec9e9e705'
     if @message.save
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      @client.account.messages.create({
+        :from => '+16572206463',
+        :to => '+17142695412',
+        :body => 'hey bro its mike from twilio',
+      })
       flash[:notice] = "Your message was sent!"
       redirect_to messages_path
     else
